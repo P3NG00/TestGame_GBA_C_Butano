e8fa25785c2fd196f86b03dbce1b0598d0e2ba3b
P3NG00 <p3ng00minecraft@gmail.com>
2023-05-20 16:21:48 -0600

    fixed projectile moving in no direction

--- include/tg_player.hpp
+++ include/tg_player.hpp
@@ -1,7 +1,7 @@
 #ifndef TG_PLAYER_HPP
 #define TG_PLAYER_HPP
 
-#include "bn_fixed.h"
+#include "bn_fixed_point.h"
 #include "bn_sprite_ptr.h"
 
 #include "bn_sprite_items_player.h"
@@ -11,8 +11,9 @@
 class player
 {
     bn::sprite_ptr _sprite_ptr = bn::sprite_items::player.create_sprite(0, 0);
-    bn::fixed _dx = 0;
-    bn::fixed _dy = 0;
+    bn::fixed_point _direction = bn::fixed_point(0, 0);
+    // initialized y to -1 to handle the player facing up at the start
+    bn::fixed_point _last_direction = bn::fixed_point(0, -1);
     int _last_sprite_index = 0;
     int _new_sprite_index = 0;
 
--- src/tg_player.cpp
+++ src/tg_player.cpp
@@ -11,57 +11,65 @@
 void player::handle_input(projectile* projectile_obj)
 {
     // player movement
-    _dx = 0;
-    _dy = 0;
+    _direction.set_x(0);
+    _direction.set_y(0);
     if (bn::keypad::up_held())
-        _dy -= 1;
+        _direction.set_y(_direction.y() - 1);
     if (bn::keypad::down_held())
-        _dy += 1;
+        _direction.set_y(_direction.y() + 1);
     if (bn::keypad::left_held())
-        _dx -= 1;
+        _direction.set_x(_direction.x() - 1);
     if (bn::keypad::right_held())
-        _dx += 1;
+        _direction.set_x(_direction.x() + 1);
 
     // find sprite index
     _new_sprite_index = -1;
-    if      (_dx ==  0 && _dy == -1)
+    if      (_direction.x() ==  0 && _direction.y() == -1)
+    {
         _new_sprite_index = 0;
-    else if (_dx ==  1 && _dy == -1)
+    }
+    else if (_direction.x() ==  1 && _direction.y() == -1)
     {
         _new_sprite_index = 1;
-        _dx = NORMALIZED;
-        _dy = -NORMALIZED;
+        _direction.set_x(NORMALIZED);
+        _direction.set_y(-NORMALIZED);
     }
-    else if (_dx ==  1 && _dy ==  0)
+    else if (_direction.x() ==  1 && _direction.y() ==  0)
+    {
         _new_sprite_index = 2;
-    else if (_dx ==  1 && _dy ==  1)
+    }
+    else if (_direction.x() ==  1 && _direction.y() ==  1)
     {
         _new_sprite_index = 3;
-        _dx = NORMALIZED;
-        _dy = NORMALIZED;
+        _direction.set_x(NORMALIZED);
+        _direction.set_y(NORMALIZED);
     }
-    else if (_dx ==  0 && _dy ==  1)
+    else if (_direction.x() ==  0 && _direction.y() ==  1)
+    {
         _new_sprite_index = 4;
-    else if (_dx == -1 && _dy ==  1)
+    }
+    else if (_direction.x() == -1 && _direction.y() ==  1)
     {
         _new_sprite_index = 5;
-        _dx = -NORMALIZED;
-        _dy = NORMALIZED;
+        _direction.set_x(-NORMALIZED);
+        _direction.set_y(NORMALIZED);
     }
-    else if (_dx == -1 && _dy ==  0)
+    else if (_direction.x() == -1 && _direction.y() ==  0)
+    {
         _new_sprite_index = 6;
-    else if (_dx == -1 && _dy == -1)
+    }
+    else if (_direction.x() == -1 && _direction.y() == -1)
     {
         _new_sprite_index = 7;
-        _dx = -NORMALIZED;
-        _dy = -NORMALIZED;
+        _direction.set_x(-NORMALIZED);
+        _direction.set_y(-NORMALIZED);
     }
 
     // update position
     if (!bn::keypad::l_held())
     {
-        _sprite_ptr.set_x(_sprite_ptr.x() + _dx);
-        _sprite_ptr.set_y(_sprite_ptr.y() + _dy);
+        _sprite_ptr.set_x(_sprite_ptr.x() + _direction.x());
+        _sprite_ptr.set_y(_sprite_ptr.y() + _direction.y());
     }
 
     // update sprite check
@@ -69,11 +77,12 @@ void player::handle_input(projectile* projectile_obj)
     {
         _sprite_ptr.set_tiles(bn::sprite_items::player.tiles_item().create_tiles(_new_sprite_index));
         _last_sprite_index = _new_sprite_index;
+        _last_direction = _direction;
     }
 
     // check player shooting
     if (bn::keypad::a_pressed())
     {
-        projectile_obj->set(_sprite_ptr.x(), _sprite_ptr.y(), _dx * 3, _dy * 3);
+        projectile_obj->set(_sprite_ptr.x(), _sprite_ptr.y(), _last_direction.x() * 3, _last_direction.y() * 3);
     }
 }
