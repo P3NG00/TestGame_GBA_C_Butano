9b37713e2e6d5cad4696d6284882de3e180411b3
P3NG00 <p3ng00minecraft@gmail.com>
2023-05-20 18:03:19 -0600

    added signature to startup scene

--- include/tg_scene_startup.hpp
+++ include/tg_scene_startup.hpp
@@ -2,15 +2,18 @@
 #define TG_SCENE_STARTUP_HPP
 
 #include "bn_regular_bg_ptr.h"
+#include "bn_sprite_ptr.h"
 
 #include "bn_regular_bg_items_p3.h"
+#include "bn_sprite_items_signature.h"
 
 #include "tg_functions.hpp"
 
 class scene_startup
 {
     bn::fixed _counter = seconds_to_frames(2);
-    bn::regular_bg_ptr _bg_logo = bn::regular_bg_items::p3.create_bg(0, 0);
+    bn::regular_bg_ptr _bg_logo = bn::regular_bg_items::p3.create_bg(-16, -16);
+    bn::sprite_ptr _sprite_signature = bn::sprite_items::signature.create_sprite(48, 48);
 
 public:
 
